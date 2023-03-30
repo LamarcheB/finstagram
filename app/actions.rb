1fde683c8d3e3682c09f25e9867126c35e256914
Ben Lamarche <lamarche.w.benjamin@gmail.com>
2023-03-30 02:53:18 +0000

    W5D2

--- app/actions.rb
+++ app/actions.rb
@@ -3,6 +3,7 @@ helpers do
     User.find_by(id: session[:user_id])
   end
 end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
@@ -50,4 +51,27 @@ end
 get '/logout' do
   session[:user_id] = nil
   redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+  # params.to_s
+  photo_url = params[:photo_url]
+
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    erb(:"finstagram_posts/new")
+  end
+end
+
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])   # find the finstagram post with the ID from the URL
+  erb(:"finstagram_posts/show")               # render app/views/finstagram_posts/show.erb
 end
